ab74c2026c9c5fbd910a11c504c1558e098670d6
Marcus Huynh <marcusjetra@gmail.com>
2025-07-22 13:34:43 -0400

    initialized components

---
+++ src/components/Board.jsx
@@ -0,0 +1,19 @@
+import Tile from "./Tile";
+import Strike from "./Strike";
+
+function Board() {
+    return <div>
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Strike />
+    </div>;
+}
+
+export default Board;
